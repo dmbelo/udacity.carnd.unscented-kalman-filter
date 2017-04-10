a705fc928cf271b338ce12be8d8b2164f65ca3a3
dmbelo <david.belo@mclaren.com>
2017-04-09 20:53:51 -0700

    Programming task 3 started

--- ukf.cpp
+++ ukf.cpp
@@ -19,6 +19,61 @@ void UKF::Init() {
 * Programming assignment functions: 
 *******************************************************************************/
 
+void UKF::GenerateSigmaPoints(MatrixXd* Xsig_out) {
+  //set state dimension
+  int n_x = 5;
+
+  //define spreading parameter
+  double lambda = 3 - n_x;
+
+  //set example state
+  VectorXd x = VectorXd(n_x);
+  x <<   5.7441,
+         1.3800,
+         2.2049,
+         0.5015,
+         0.3528;
+
+  //set example covariance matrix
+  MatrixXd P = MatrixXd(n_x, n_x);
+  P <<     0.0043,   -0.0013,    0.0030,   -0.0022,   -0.0020,
+          -0.0013,    0.0077,    0.0011,    0.0071,    0.0060,
+           0.0030,    0.0011,    0.0054,    0.0007,    0.0008,
+          -0.0022,    0.0071,    0.0007,    0.0098,    0.0100,
+          -0.0020,    0.0060,    0.0008,    0.0100,    0.0123;
+
+  //create sigma point matrix
+  MatrixXd Xsig = MatrixXd(n_x, 2 * n_x + 1);
+
+  //calculate square root of P
+  MatrixXd A = P.llt().matrixL();
+
+  /*******************************************************************************
+   * Student part begin
+  ******************************************************************************/
+
+  //set first column of sigma point matrix
+  Xsig.col(0)  = x;
+
+  //set remaining sigma points
+  for (int i = 0; i < n_x; i++)
+  {
+    Xsig.col(i+1)     = x + sqrt(lambda+n_x) * A.col(i);
+    Xsig.col(i+1+n_x) = x - sqrt(lambda+n_x) * A.col(i);
+  }
+
+  /*******************************************************************************
+   * Student part end
+  ******************************************************************************/
+
+  //print result
+  //std::cout << "Xsig = " << std::endl << Xsig << std::endl;
+
+  //write result
+  *Xsig_out = Xsig;
+
+}
+
 void UKF::AugmentedSigmaPoints(MatrixXd* Xsig_out) {
 
   //set state dimension
@@ -61,9 +116,9 @@ void UKF::AugmentedSigmaPoints(MatrixXd* Xsig_out) {
   //create sigma point matrix
   MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);
 
-/*******************************************************************************
- * Student part begin
- ******************************************************************************/
+  /*******************************************************************************
+   * Student part begin
+   ******************************************************************************/
  
   //create augmented mean state (last two elements are zero bc noise is zero mean)
   x_aug.head(5) = x;
@@ -96,9 +151,9 @@ void UKF::AugmentedSigmaPoints(MatrixXd* Xsig_out) {
   Xsig_aug.col(14) = x_aug - tmp.col(6);
 
   
-/*******************************************************************************
- * Student part end
- ******************************************************************************/ 
+  /*******************************************************************************
+   * Student part end
+   ******************************************************************************/ 
   
   //print result
   std::cout << "Xsig_aug = " << std::endl << Xsig_aug << std::endl;
@@ -106,7 +161,7 @@ void UKF::AugmentedSigmaPoints(MatrixXd* Xsig_out) {
   //write result
   *Xsig_out = Xsig_aug;
 
-/* expected result:
+  /* expected result:
    Xsig_aug =
   5.7441  5.85768   5.7441   5.7441   5.7441   5.7441   5.7441   5.7441  5.63052   5.7441   5.7441   5.7441   5.7441   5.7441   5.7441
     1.38  1.34566  1.52806     1.38     1.38     1.38     1.38     1.38  1.41434  1.23194     1.38     1.38     1.38     1.38     1.38
@@ -115,6 +170,50 @@ void UKF::AugmentedSigmaPoints(MatrixXd* Xsig_out) {
   0.3528 0.299973 0.462123 0.376339  0.48417 0.418721   0.3528   0.3528 0.405627 0.243477 0.329261  0.22143 0.286879   0.3528   0.3528
        0        0        0        0        0        0  0.34641        0        0        0        0        0        0 -0.34641        0
        0        0        0        0        0        0        0  0.34641        0        0        0        0        0        0 -0.34641
-*/
+  */
+
+}
+
+void UKF::SigmaPointPrediction(MatrixXd* Xsig_out) {
+
+  //set state dimension
+  int n_x = 5;
+
+  //set augmented dimension
+  int n_aug = 7;
+
+  //create example sigma point matrix
+  MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);
+  Xsig_aug <<
+    5.7441,  5.85768,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.63052,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,   5.7441,
+      1.38,  1.34566,  1.52806,     1.38,     1.38,     1.38,     1.38,     1.38,   1.41434,  1.23194,     1.38,     1.38,     1.38,     1.38,     1.38,
+    2.2049,  2.28414,  2.24557,  2.29582,   2.2049,   2.2049,   2.2049,   2.2049,   2.12566,  2.16423,  2.11398,   2.2049,   2.2049,   2.2049,   2.2049,
+    0.5015,  0.44339, 0.631886, 0.516923, 0.595227,   0.5015,   0.5015,   0.5015,   0.55961, 0.371114, 0.486077, 0.407773,   0.5015,   0.5015,   0.5015,
+    0.3528, 0.299973, 0.462123, 0.376339,  0.48417, 0.418721,   0.3528,   0.3528,  0.405627, 0.243477, 0.329261,  0.22143, 0.286879,   0.3528,   0.3528,
+         0,        0,        0,        0,        0,        0,  0.34641,        0,         0,        0,        0,        0,        0, -0.34641,        0,
+         0,        0,        0,        0,        0,        0,        0,  0.34641,         0,        0,        0,        0,        0,        0, -0.34641;
+
+  //create matrix with predicted sigma points as columns
+  MatrixXd Xsig_pred = MatrixXd(n_x, 2 * n_aug + 1);
+
+  double delta_t = 0.1; //time diff in sec
+  /*******************************************************************************
+   * Student part begin
+  ******************************************************************************/
+
+  //predict sigma points
+  //avoid division by zero
+  //write predicted sigma points into right column
+  
+
+  /*******************************************************************************
+   * Student part end
+  ******************************************************************************/
+
+  //print result
+  std::cout << "Xsig_pred = " << std::endl << Xsig_pred << std::endl;
+
+  //write result
+  *Xsig_out = Xsig_pred;
 
 }
