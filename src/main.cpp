0cf19b59c75e01b8da5ac76351a66157cd14859d
dmbelo <david.belo@mclaren.com>
2017-04-15 16:34:59 -0700

    Added gt and measurement packages

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include <fstream>
 #include <iostream>
-#include "Eigen/Dense"
 #include <vector>
+
+#include "Eigen/Dense"
 #include "ukf.h"
+#include "ground_truth_package.h"
+#include "measurement_package.h"
 
 using namespace std;
-using Eigen::MatrixXd;
-using Eigen::VectorXd;
-using std::vector;
+// using Eigen::MatrixXd;
+// using Eigen::VectorXd;
+// using std::vector;
 
 void check_arguments(int argc, char* argv[]) {
   string usage_instructions = "Usage instructions: ";
@@ -45,19 +48,30 @@ void check_files(ifstream& in_file, string& in_name,
   }
 }
 
+int main(int argc, char* argv[]) {
+	
+	check_arguments(argc, argv);
 
-int main() {
+	string in_file_name_ = argv[1];
+	ifstream in_file_(in_file_name_.c_str(), ifstream::in);
 
-	//Create a UKF instance
-	UKF ukf(5, 0.2, 0.2);
-	cout << ukf.lambda << endl;
+	string out_file_name_ = argv[2];
+	ofstream out_file_(out_file_name_.c_str(), ofstream::out);
 
-/*******************************************************************************
-* Programming assignment calls
-*******************************************************************************/
+	check_files(in_file_, in_file_name_, out_file_, out_file_name_);
 
-    // MatrixXd Xsig_pred = MatrixXd(15, 5);
-    // ukf.SigmaPointPrediction(&Xsig_pred);
+	/**********************************************
+	*  Set Measurements                          *
+	**********************************************/
+
+	vector<MeasurementPackage> measurement_pack_list;
+	vector<GroundTruthPackage> gt_pack_list;
+
+	string line;
+
+	// prep the measurement packages (each line represents a measurement at a
+	// timestamp)
+
 
 	return 0;
 }
