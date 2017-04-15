eb9bd44efce983c1c68f5975598be61c59339724
dmbelo <david.belo@mclaren.com>
2017-04-15 16:29:34 -0700

    Added argument check

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 #include "Eigen/Dense"
 #include <vector>
@@ -7,6 +8,43 @@ using namespace std;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 using std::vector;
+
+void check_arguments(int argc, char* argv[]) {
+  string usage_instructions = "Usage instructions: ";
+  usage_instructions += argv[0];
+  usage_instructions += " path/to/input.txt output.txt";
+
+  bool has_valid_args = false;
+
+  // make sure the user has provided input and output files
+  if (argc == 1) {
+    cerr << usage_instructions << endl;
+  } else if (argc == 2) {
+    cerr << "Please include an output file.\n" << usage_instructions << endl;
+  } else if (argc == 3) {
+    has_valid_args = true;
+  } else if (argc > 3) {
+    cerr << "Too many arguments.\n" << usage_instructions << endl;
+  }
+
+  if (!has_valid_args) {
+    exit(EXIT_FAILURE);
+  }
+}
+
+void check_files(ifstream& in_file, string& in_name,
+                 ofstream& out_file, string& out_name) {
+  if (!in_file.is_open()) {
+    cerr << "Cannot open input file: " << in_name << endl;
+    exit(EXIT_FAILURE);
+  }
+
+  if (!out_file.is_open()) {
+    cerr << "Cannot open output file: " << out_name << endl;
+    exit(EXIT_FAILURE);
+  }
+}
+
 
 int main() {
 
