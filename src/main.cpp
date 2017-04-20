4f238a40de20446e696981836632bc9a184d9450
dmbelo <david.belo@mclaren.com>
2017-04-19 20:20:07 -0700

    Finishing the lidar implementation

--- src/main.cpp
+++ src/main.cpp
@@ -204,24 +204,19 @@ int main(int argc, char *argv[])
 	    // output the estimation
 
 	    // p1 - meas
-	    // out_file_ << measurement_pack_list[k].raw_measurements_(0) << "\t";
+	    out_file_ << measurement_pack_list[k].raw_measurements_(0) << "\t";
 
 	    // p2 - meas
-	    // out_file_ << measurement_pack_list[k].raw_measurements_(1) << "\t";
+	    out_file_ << measurement_pack_list[k].raw_measurements_(1) << "\t";
 	}
 	else if (measurement_pack_list[k].sensor_type_ == MeasurementPackage::RADAR)
 	{
 	    // output the estimation in the cartesian coordinates
 	    float ro = measurement_pack_list[k].raw_measurements_(0);
 	    float phi = measurement_pack_list[k].raw_measurements_(1);
-	    // out_file_ << ro * cos(phi) << "\t"; // p1_meas
-	    // out_file_ << ro * sin(phi) << "\t"; // p2_meas
+	    out_file_ << ro * cos(phi) << "\t"; // p1_meas
+	    out_file_ << ro * sin(phi) << "\t"; // p2_meas
 	}
-
-	float ro = measurement_pack_list[k].raw_measurements_(0);
-	float phi = measurement_pack_list[k].raw_measurements_(1);
-	out_file_ << ro * cos(phi) << "\t"; // p1_meas
-	out_file_ << ro * sin(phi) << "\t"; // p2_meas
 
 	// output the ground truth packages
 	out_file_ << gt_pack_list[k].gt_values_(0) << "\t";
